06da225ae20bfa350761686c06c2c29a8d73c283
Vidhula C <cvidhula06@gmail.com>
2026-08-11 16:30:55 +0530

    Add operators practice

---
+++ basics/operators.py
@@ -0,0 +1,29 @@
+a=int(input("enter the 1st number:"))
+b=int(input("enter the 2nd number:"))
+
+print(f"ADDITION of {a} and {b}")
+print(f"SUBTRACTION of {a} and {b}")
+print(f"MULTIPLICATION of {a} and {b}")
+print(f"ADDITION of {a} and {b}")
+print(f"DIVISON of {a} and {b}")
+print(f"FLOOR DIVISION of {a} and {b}")
+print(f"REMAINDER of {a} and {b}")
+print(f"POWER of {a} and {b}")
+
+#Comparison operators
+
+print(f"GREATER THAN: {a} > {b} = {a > b}")
+print(f"LESS THAN: {a} < {b} = {a < b}")
+print(f"EQUAL TO: {a} == {b} = {a == b}")
+print(f"NOT EQUAL TO: {a} != {b} = {a != b}")
+print(f"GREATER THAN OR EQUAL TO: {a} >= {b} = {a >= b}")
+print(f"LESS THAN OR EQUAL TO: {a} <= {b} = {a <= b}")
+
+# Logical operators
+x = True
+y = False
+
+print(x and y)
+print(x or y)
+print(not x)
+
