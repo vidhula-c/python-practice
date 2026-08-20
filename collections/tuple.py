cc6eed38b046f8df4e8b674a62fa1fbb7559fca1
Vidhula C <cvidhula06@gmail.com>
2026-08-20 21:27:43 +0530

    Add Python tuple basics

---
+++ collections/tuple.py
@@ -0,0 +1,18 @@
+# Creating a tuple
+fruits = ("apple", "banana", "mango")
+
+print(fruits)
+
+# Accessing elements
+print(fruits[0])
+print(fruits[1])
+
+# Length of tuple
+print(len(fruits))
+
+# Checking if an element exists
+print("apple" in fruits)
+
+# Looping through a tuple
+for fruit in fruits:
+    print(fruit)
